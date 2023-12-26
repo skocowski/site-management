945c60423bf7c09faa455a74f7767f845cd2f33e
Szymon Kocowski <skocowski@icloud.com>
2023-12-26 20:29:00 +0700

    first commit

--- app/utils/Constants.ts
+++ app/utils/Constants.ts
@@ -1,7 +1,3 @@
 
 
 
-
-export const contractAddress = "0xB4B6852EA0d8a8037Be55aFE2BA9c8373Dd67380";
- export const goldTokenAddress = "0x972937F709985f248a5F1E161c5F68F5a0521eB4";
-//export const contractAddress = "0xA4f5043d319b36e00E771B44C96a88Bad7a72C3F" 
