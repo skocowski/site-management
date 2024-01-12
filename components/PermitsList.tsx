3e7327fedb96d95dcc32fd8b0748d3e32be22a90
Szymon Kocowski <skocowski@icloud.com>
2024-01-12 14:06:26 +0700

    next

--- components/PermitsList.tsx
+++ components/PermitsList.tsx
@@ -14,22 +14,7 @@ const PermitsList = () => {
     <div>
       <h2>Permits</h2>
      
-      <ul>
-        {permits.map((permit: Permit, index: any) => (
-          <li key={index}>
-            <h3>{permit.applicant}</h3>
-            <p>Badge: {permit.badge}</p>
-            <p>Company: {permit.company}</p>
-            <p>Contact Number: {permit.contactNumber}</p>
-     {/*        <p>Date: {permit.date.toDate().toLocaleDateString()}</p> */}
-            <p>Description: {permit.description}</p>
-            <p>Isolation: {permit.isolation}</p>
-            <p>Location: {permit.location}</p>
-            <p>Rams: {permit.rams}</p>
-            <p>Type: {permit.type}</p>
-          </li>
-        ))}
-      </ul>
+
     </div> 
   )
 }
